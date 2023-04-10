6ca169f32cdf654e305bff78de901d14958dc168
Diana Khlomko <diana.hlomko@gmail.com>
2023-04-10 18:51:48 +0300

    update simplecov gem

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
-require "simplecov"
-SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
+
+require "simplecov"
+SimpleCov.start "rails"
+
 require_relative "../config/environment"
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
