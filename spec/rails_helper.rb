1bba92d7275c05f86a4e05a89a0105daa4b32f22
Diana Khlomko <diana.hlomko@gmail.com>
2023-04-10 13:36:01 +0300

    add users controller add authentication controller update Gemfile with simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
