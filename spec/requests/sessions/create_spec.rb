4b64abb0adf2398da1eefc68f303b3ae39c94ce7
Diana Khlomko <diana.hlomko@gmail.com>
2023-04-11 17:03:16 +0300

    create sessions controller add rspec request tests for create and destroy actions add rspec request test for users show action update html.erb templates

---
+++ spec/requests/sessions/create_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+RSpec.describe "Sessions", type: :request do
+  let(:user) { create(:user) }
+
+  describe "POST /create" do
+    context "when user is valid" do
+      before do
+        post sessions_url, params: { email: user.email, password: user.password }
+      end
+
+      it { expect(user).to be_valid }
+
+      it "returns status code 302" do
+        expect(response).to have_http_status(:found)
+      end
+
+      it "redirects users/id" do
+        expect(response.body).to include(user_url(user.id))
+      end
+    end
+
+    context "when user is invalid" do
+      before do
+        post sessions_url, params: { email: nil, password: nil }
+      end
+
+      it "returns status code 302" do
+        expect(response).to have_http_status(:found)
+      end
+
+      it "redirects to login_url" do
+        expect(response.body).to include(new_session_url)
+      end
+    end
+  end
+end
