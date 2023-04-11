8b55c2c35bc3fd9343678a3ddbb69845b545c326
Diana Khlomko <diana.hlomko@gmail.com>
2023-04-11 16:13:07 +0300

    add users controller with authentication concern update routes.rb

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class UsersController < AuthenticationController
+  before_action :authorized, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to new_session_url
+    else
+      redirect_to new_user_url
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+  end
+end
