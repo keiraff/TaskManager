4c7d333ac420b18fa932dac60ff4d71d79ef467e
Diana Khlomko <diana.hlomko@gmail.com>
2023-05-02 19:42:54 +0300

    update users/show_spec.rb with shared example

--- spec/requests/users/show_spec.rb
+++ spec/requests/users/show_spec.rb
@@ -1,21 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe "GET /users/:id", type: :request do
-  context "when user authenticated" do
-    let(:user) { create(:user) }
-
-    before do
-      login(user)
-    end
-
-    it "returns success responce" do
-      get "/users/#{user.id}"
-
-      expect(response).to have_http_status(:ok)
-      expect(response.content_type).to eq("text/html; charset=utf-8")
-      expect(response.body).to include("Welcome to the page")
-    end
-  end
+  it_behaves_like "authenticated user request", :get, "/users/1", "Welcome to the page"
 
   it_behaves_like "unauthenticated user request", :get, "/users/1"
 end
