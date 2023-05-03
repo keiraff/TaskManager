1abe89ac7b8efbcb4b9eb5512b736725e2d17ab8
Diana Khlomko <diana.hlomko@gmail.com>
2023-05-03 21:46:46 +0300

    implement categories new page update templates add request rspecs update routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,8 @@ class CategoriesController < AuthenticatedController
   def index
     @pagy, @categories = pagy(current_user.categories, items: 10)
   end
+
+  def new
+    @category = Category.new
+  end
 end
