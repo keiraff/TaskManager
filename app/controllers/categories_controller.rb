361fbec445c37764693d9c67a7aa71e5631564c4
Diana Khlomko <diana.hlomko@gmail.com>
2023-05-04 14:09:23 +0300

    add categories create action update templates add request rspecs update routes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -8,4 +8,22 @@ class CategoriesController < AuthenticatedController
   def new
     @category = Category.new
   end
+
+  def create
+    @category = current_user.categories.new(category_params)
+
+    if @category.save
+      flash[:success] = "Category created!"
+      redirect_to categories_url
+    else
+      flash[:error] = "Something went wrong :("
+      render "new"
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
