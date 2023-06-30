ad9cb2d411b2466615ec3df83013e37875db417d
Diana Khlomko <diana.hlomko@gmail.com>
2023-06-30 13:31:17 +0300

    add event index action

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class EventsController < AuthenticatedController
+
+  def index
+    @search_result = events_scope.ransack(params[:query])
+    @search_result.sorts = ["date asc"] if @search_result.sorts.empty?
+    @pagy, @events = pagy(@search_result.result.includes(:category))
+  end
+
+  def show
+    @event = event
+  end
+
+  private
+
+  def event
+    @event ||= events_scope.find(params[:id])
+  end
+
+  def events_scope
+    current_user.events
+  end
+end
