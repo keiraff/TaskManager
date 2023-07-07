78e0a5bc74f3d280eaac3281ae60008abaa09a4d
Diana Khlomko <diana.hlomko@gmail.com>
2023-07-07 15:12:05 +0300

    add events index and show request specs update templates

---
+++ spec/requests/events/show_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+RSpec.describe "GET /events/:id", type: :request do
+  context "when user authenticated" do
+    let(:user) { create(:user) }
+    let(:category) { create(:category, name: "Personal", user: user) }
+    let(:event) { create(:event, name: "Trip to LA", user: user, category: category) }
+
+    before do
+      login(user)
+    end
+
+    it "returns success responce" do
+      get "/events/#{event.id}"
+
+      expect(response).to have_http_status(:ok)
+      expect(response.body).to include(event.name, event.description)
+    end
+  end
+
+  it_behaves_like "unauthenticated user request", :get, "/events/1"
+end
