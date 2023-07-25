6a4dcc48926ca0b77bff47de00a366dd4de4ec62
Diana Khlomko <diana.hlomko@gmail.com>
2023-07-25 14:08:32 +0300

    update custom js

---
+++ app/javascript/events/new.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+    if(!$('#events_new_form')) return;
+
+    $('#ends_at_time select').prop('disabled', true);
+
+    // Disable ends_at selects when all_day checkbox is clicked
+    $("#all_day_checkbox[type='checkbox']").on('change', function() {
+        if($(this).prop('checked')) {
+            $('#ends_at_time select').prop('disabled', true);
+        } else {
+            $('#ends_at_time select').prop('disabled', false);
+        }
+    });
+
+    $('#notification_time select').prop('disabled', true);
+
+    // Enable notify_at selects when notification checkbox is clicked
+    $("#notify_at_checkbox[type='checkbox']").on('change', function() {
+        if($(this).prop('checked')) {
+            $('#notification_time select').prop('disabled', false);
+        } else {
+            $('#notification_time select').prop('disabled', true);
+        }
+    });
+});
