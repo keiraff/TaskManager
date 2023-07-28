ffbcf1958423754d84bf9a97828976001d54d77a
Diana Khlomko <diana.hlomko@gmail.com>
2023-07-28 17:19:45 +0300

    update jquery scripts

--- app/javascript/events/edit.js
+++ app/javascript/events/edit.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-    if ($('#edit_event_form').length) {
+$(document).ready(function() {
+    if($('#edit_event_form').length) {
 
         // Check all_day checkbox when page load
-        if ($("#all_day_checkbox[type='checkbox']").prop('checked')) {
+        if($("#all_day_checkbox[type='checkbox']").prop('checked')) {
             $('#ends_at_time').hide();
         }
 
         // Hide ends_at selects when all_day checkbox is checked
-        $("#all_day_checkbox[type='checkbox']").on('change', function () {
-            if ($(this).prop('checked')) {
+        $("#all_day_checkbox[type='checkbox']").on('change', function() {
+            if($(this).prop('checked')) {
                 $('#ends_at_time select option').removeAttr('selected');
                 $('#ends_at_time select').val("");
                 $('#ends_at_time').hide();
@@ -18,13 +18,13 @@ $(document).ready(function () {
         });
 
         // Check notify_at checkbox when page load
-        if (!$("#notify_at_checkbox[type='checkbox']").prop('checked')) {
+        if(!$("#notify_at_checkbox[type='checkbox']").prop('checked')) {
             $('#notification_time').hide();
         }
 
         // Hide notify_at selects when notification checkbox is not clicked
-        $("#notify_at_checkbox[type='checkbox']").on('change', function () {
-            if ($(this).prop('checked')) {
+        $("#notify_at_checkbox[type='checkbox']").on('change', function() {
+            if($(this).prop('checked')) {
                 $('#notification_time select').show();
             } else {
                 $('#notification_time select option').removeAttr('selected');
--- app/javascript/events/new.js
+++ app/javascript/events/new.js
@@ -1,14 +1,14 @@
-$(document).ready(function () {
-    if ($('#new_event_form').length) {
+$(document).ready(function() {
+    if($('#new_event_form').length) {
 
         // Check all_day checkbox when page load
-        if ($("#all_day_checkbox[type='checkbox']").prop('checked')) {
+        if($("#all_day_checkbox[type='checkbox']").prop('checked')) {
             $('#ends_at_time').hide();
         }
 
         // Disable ends_at selects when all_day checkbox is clicked
-        $("#all_day_checkbox[type='checkbox']").on('change', function () {
-            if ($(this).prop('checked')) {
+        $("#all_day_checkbox[type='checkbox']").on('change', function() {
+            if($(this).prop('checked')) {
                 $('#ends_at_time select').val(null);
                 // Set starts_at time selects to 00:00
                 $('#event_starts_at_4i').val("00");
@@ -23,8 +23,8 @@ $(document).ready(function () {
         $('#notification_time').hide();
 
         // Enable notify_at selects when notification checkbox is clicked
-        $("#notify_at_checkbox[type='checkbox']").on('change', function () {
-            if ($(this).prop('checked')) {
+        $("#notify_at_checkbox[type='checkbox']").on('change', function() {
+            if($(this).prop('checked')) {
                 $('#notification_time').show();
             } else {
                 $('#notification_time select').val(null);
