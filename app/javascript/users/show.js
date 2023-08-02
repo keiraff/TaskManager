e51f0f7f779691bc66c37cedf0cd7664bc4a1688
Diana Khlomko <diana.hlomko@gmail.com>
2023-08-02 17:53:30 +0300

    update users show jquery script

--- app/javascript/users/show.js
+++ app/javascript/users/show.js
@@ -1,21 +1,14 @@
 $(document).ready(function() {
-    if($('#users_show').length) {
-
-        if(localStorage["active_tab"]) {
-            var activeTab = localStorage["active_tab"];
-            $(`${activeTab}_tab, ${activeTab}`).addClass("active");
-        }
-
-        $("#today_events_tab").on("click", function(event) {
-            $("#current_events_tab, #current_events").removeClass("active");
-            $("#today_events_tab, #today_events").addClass("active");
-            localStorage["active_tab"] = "#today_events";
-        });
-
-        $("#current_events_tab").on("click", function(event) {
-            $("#today_events_tab, #today_events").removeClass("active");
-            $("#current_events_tab, #current_events").addClass("active");
-            localStorage["active_tab"] = "#current_events";
-        });
+    if(!$('#users_show').length) {
+        return;
     }
+
+    if(localStorage["active_tab"]) {
+        $(`${localStorage["active_tab"]}`).tab("show");
+    }
+
+    $("#today_events_tab, #current_events_tab").on("click", function(event) {
+        localStorage["active_tab"] = `#${event.target.id}`;
+        $(`${localStorage["active_tab"]}`).tab("show");
+    });
 });
