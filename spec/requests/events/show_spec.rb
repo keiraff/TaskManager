6609a7d51d1f44137c7ca4cc9d3f32bbab4f6231
Diana Khlomko <diana.hlomko@gmail.com>
2023-08-15 20:37:13 +0300

    update events show spec add vcr cassettes for geocoder and external api requests in specs

--- spec/requests/events/show_spec.rb
+++ spec/requests/events/show_spec.rb
@@ -2,19 +2,73 @@
 
 RSpec.describe "GET /events/:id", type: :request do
   context "when user authenticated" do
-    let(:user) { create(:user) }
-    let(:category) { create(:category, user: user) }
-    let(:event) { create(:event, user: user, category: category) }
+    context "when user location is set" do
+      let(:user) { create(:user, city: "New York", state: "NY", country: "US") }
+      let(:category) { create(:category, user: user) }
 
-    before do
-      login(user)
+      before do
+        login(user)
+      end
+
+      context "when get response with OK status" do
+        context "when get valid data from external api" do
+          let(:event) { create(:event, user: user, category: category) }
+
+          it "returns success response", vcr: "weather_api_valid_response_data" do
+            get "/events/#{event.id}"
+
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include("Temperature", "Precipitation probability")
+          end
+        end
+
+        context "when get invalid data from external api" do
+          let(:event) { build(:event, starts_at: 12.weeks.ago, user: user, category: category) }
+
+          before do
+            event.save(validate: false)
+          end
+
+          it "returns error", vcr: "weather_api_invalid_response_data" do
+            get "/events/#{event.id}"
+
+            expect(response).to have_http_status(:ok)
+            expect(response.body).not_to include("Temperature", "Precipitation probability")
+            expect(response.body).to include("Can't show forecast for this event.")
+          end
+        end
+      end
+
+      context "when API request error" do
+        let(:event) { create(:event, starts_at: 17.days.from_now, user: user, category: category) }
+
+        it "returns error", vcr: "weather_api_request_error" do
+          get "/events/#{event.id}"
+
+          expect(response).to have_http_status(:ok)
+          expect(response.body).not_to include("Temperature", "Precipitation probability")
+          expect(response.body).to include("Can't show forecast for this event. API request error.")
+        end
+      end
     end
 
-    it "returns success responce" do
-      get "/events/#{event.id}"
+    context "when user location is blank" do
+      let(:user) { create(:user) }
+      let(:category) { create(:category, user: user) }
+      let(:event) { create(:event, user: user, category: category) }
 
-      expect(response).to have_http_status(:ok)
-      expect(response.body).to include(event.name, event.description)
+      before do
+        login(user)
+      end
+
+      it "returns success responce" do
+        get "/events/#{event.id}"
+
+        expect(response).to have_http_status(:ok)
+        expect(response.body).to include(event.name, event.description,
+                                         "Can't show forecast for this event. Specify your location in Settings.")
+        expect(response.body).not_to include("Temperature", "Precipitation probability")
+      end
     end
   end
 
