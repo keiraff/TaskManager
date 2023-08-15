e111658c466d0260a41d488311f329c9c25da144
Diana Khlomko <diana.hlomko@gmail.com>
2023-08-15 14:35:19 +0300

    add location get spec

---
+++ spec/requests/location/get_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+RSpec.describe "GET /location", type: :request do
+  context "when user authenticated" do
+    let(:user) { create(:user) }
+
+    before do
+      login(user)
+    end
+
+    context "with valid params" do
+      let(:attributes) do
+        {
+          country: "US",
+          state: "NY",
+        }
+      end
+
+      it "returns success responce" do
+        get "/location", xhr: true, params: attributes
+
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        expect(response.body).to include(CS.states(attributes[:country]).first[1],
+                                         CS.cities(attributes[:state], attributes[:country]).first[1])
+      end
+    end
+
+    context "with invalid params" do
+      let(:attributes) do
+        {
+          country: nil,
+          state: nil,
+        }
+      end
+
+      it "returns success responce" do
+        get "/location", xhr: true, params: attributes
+
+        expect(response).to have_http_status(:ok)
+        expect(response.content_type).to eq("application/json; charset=utf-8")
+        expect(response.body).to include("{\"states\":null,\"cities\":null}")
+      end
+    end
+  end
+
+  it_behaves_like "unauthenticated user request", :get, "/location"
+end
